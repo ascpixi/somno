ff74bee9a44eef11c54a46ee14ee89c7b9fb2068
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-07-15 02:20:51 +0200

    lots of stuff! *★,°*:.☆(￣▽￣)/$:*.°★* 。

---
+++ src/Somno.Portal/DLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somno.Portal
+{
+    internal class DLL
+    {
+        private const uint DLL_PROCESS_DETACH = 0,
+                           DLL_PROCESS_ATTACH = 1,
+                           DLL_THREAD_ATTACH = 2,
+                           DLL_THREAD_DETACH = 3;
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        [UnmanagedCallersOnly(EntryPoint = "DllMain", CallConvs = new[] { typeof(CallConvStdcall) })]
+        public static bool DllMain(IntPtr hModule, uint ul_reason_for_call, IntPtr lpReserved)
+        {
+            switch (ul_reason_for_call) {
+                case DLL_PROCESS_ATTACH:
+                    MessageBox(0, "DLL_PROCESS_ATTACH", "Your kind @", 0);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+}

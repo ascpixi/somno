cf9bf588941c586aa8c2fb31a4d4a459b47f853c
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-07-18 18:08:41 +0200

    Return if build failed

--- src/build.bat
+++ src/build.bat
@@ -3,22 +3,31 @@ call env.bat
 
 cd Somno.ILTransformer
 dotnet publish -r win-x64 -c %1
+if %errorlevel% neq 0 goto fail
 cd ..
 
 cd Somno.Packager
 dotnet publish -r win-x64 -c %1 -p:IsCLI=true
+if %errorlevel% neq 0 goto fail
 cd ..
 
 cd Somno.Portal
 "%MSBUILD_PATH%" Somno.Portal.vcxproj /p:Configuration=%1 /p:Platform=x64
+if %errorlevel% neq 0 goto fail
 cd ..
 
 cd Somno
 dotnet publish -r win-x64 -c %1 /p:RepackagePortalAgent=true
+if %errorlevel% neq 0 goto fail
 cd ..
 
 echo.
 echo.
-echo.
-echo.
 echo Build done!
+goto :EOF
+
+:fail
+    cd ..
+    echo.
+    echo.
+    echo Build failed.

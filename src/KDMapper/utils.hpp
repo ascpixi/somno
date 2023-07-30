046a8d19e0776fd9b5cb65f5b0e981be71be6dc1
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-07-30 15:24:33 +0200

    Add KDMapper

---
+++ src/KDMapper/utils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#define DISABLE_OUTPUT
+
+#if defined(DISABLE_OUTPUT)
+	#define Log(content) 
+#else
+	#define Log(content) std::wcout << content
+#endif
+
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "nt.hpp"
+
+namespace utils
+{
+	std::wstring GetFullTempPath();
+	bool CreateFileFromMemory(const std::wstring& desired_file_path, const char* address, size_t size);
+	uint64_t GetKernelModuleAddress(const std::string& module_name);
+	BOOLEAN bDataCompare(const BYTE* pData, const BYTE* bMask, const char* szMask);
+	uintptr_t FindPattern(uintptr_t dwAddress, uintptr_t dwLen, BYTE* bMask, char* szMask);
+	PVOID FindSection(char* sectionName, uintptr_t modulePtr, PULONG size);
+}

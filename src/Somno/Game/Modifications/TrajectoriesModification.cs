6ba320a0a44bee0eabfcc9dd8706fd765ffc2412
ascpixi <44982772+ascpixi@users.noreply.github.com>
2023-08-02 17:53:17 +0200

    Create scaffold for ESP + trajectories + waypoints

---
+++ src/Somno/Game/Modifications/TrajectoriesModification.cs
@@ -0,0 +1,33 @@
+﻿using ImGuiNET;
+using Somno.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somno.Game.Modifications
+{
+    internal class TrajectoriesModification : IOverlayRenderable, IConfigRenderable
+    {
+        public bool Enabled;
+
+        public void RenderConfiguration(SomnoOverlay overlay)
+        {
+            if (ImGui.CollapsingHeader("Trajectories")) {
+                ImGui.Text("Renders the trajectories of grenades.");
+                ImGui.Indent(12);
+
+                ImGui.Checkbox("Enabled", ref Enabled);
+
+                ImGui.Unindent(12);
+            }
+        }
+
+        public void RenderOnOverlay(SomnoOverlay overlay)
+        {
+            // ...
+        }
+    }
+}
